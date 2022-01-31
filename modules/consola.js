3d451a0bd54a1465ce0258e80d46a5cbd06ee0dd
Antonio Humanes <antoniohumanespacheco@gmail.com>
2022-01-31 18:25:53 +0100

    Clase: Consola

---
+++ modules/consola.js
@@ -0,0 +1,24 @@
+const tabla = [{
+    a: 1,
+    b: 2
+}, {
+    a: 3,
+    b: 4
+}];
+
+console.table(tabla)
+
+
+
+console.group('conver')
+    console.log('Hola');
+    console.log('bla bla bla...');
+    console.log('adios');
+console.groupEnd('conver')
+
+
+console.count('veces');
+console.count('veces');
+console.count('veces');
+console.count('veces');
+console.count('veces');
