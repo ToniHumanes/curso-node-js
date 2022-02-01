32deb2af4330c5682a5a4b75eeb5adf4ed3934cb
Antonio Humanes <antoniohumanespacheco@gmail.com>
2022-02-01 20:01:42 +0100

    Clase: http

---
+++ modules/http.js
@@ -0,0 +1,26 @@
+const http = require('http');
+http.createServer(router).listen(3000);
+
+
+function router(request, response) {
+    console.log('nueva peticion');
+    console.log(request.url);
+
+    switch (request.url) {
+        case '/hola':
+            response.write('Hola, que tal?');
+            response.end();
+            break;
+
+        default:
+            response.write('Error 404, no se que quieres')
+            response.end();
+            break;
+    }
+
+    // response.writeHead(201, {'Content-Type': 'text/plain'})
+    // response.write('Hola, ya se usar http de nodejs')
+
+}
+
+console.log('Escuchando http en el puerto 300')
